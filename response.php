48074bb29f7fbadd22621a1c997cb3fa29423c82
Will <wrsmith865@gmail.com>
2022-03-01 19:58:49 +0000

    actual file

---
+++ response.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+<TITLE> response </TITLE>
+</HEAD>
+<BODY>
+Your name is: <?php echo $_POST ['name'];?> <BR/>
+Your email is: <?php echo $_POST ['email'];?> <BR/>
+Your Phone number: <?php echo $_POST ['phone'];?> <BR/>
+Your Comments: <i><?php echo $_POST ['comments'];?> </i>
+</BODY>
+</HTML>
